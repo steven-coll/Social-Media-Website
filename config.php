6c6d9d73c384336a1da4c41b11c529cd0adc1f00
steven-coll <42589270+steven-coll@users.noreply.github.com>
2020-10-07 16:38:22 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'mydb.cspfaxcpufsh.us-east-2.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', '!St3v3n19');
+define('DB_PASSWORD', '00000');
 define('DB_NAME', 'mydb');
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
